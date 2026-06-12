feee19bd16ecfcd2480cc66f55186e1feabb0adb
Khalid Khan <shhkhan33@gmail.com>
2026-06-12 11:09:34 +0530

    basic Crud is written for studentmodule

--- PageFlow/src/main/java/com/PageFlow/controller/StudentController.java
+++ PageFlow/src/main/java/com/PageFlow/controller/StudentController.java
@@ -1,9 +1,15 @@
 package com.PageFlow.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,6 +36,56 @@ public class StudentController {
 		res.setData(studentService.saveStudent(student));
 
 		return ResponseEntity.status(HttpStatus.CREATED).body(res);
+	}
+
+	// fetch all student
+	@GetMapping
+	public ResponseEntity<ResponseStructure<List<Student>>> getAllStudents() {
+		List<Student> students = studentService.getAllStudents();
+		ResponseStructure<List<Student>> res = new ResponseStructure<>();
+		res.setStatusCode(HttpStatus.OK.value());
+		res.setMessage("All students fetched");
+		res.setData(students);
+
+		return new ResponseEntity<>(res, HttpStatus.OK);
+	}
+
+	// fetch by student id
+	@GetMapping("/{id}")
+	public ResponseEntity<ResponseStructure<Student>> getStudentById(@PathVariable Integer id) {
+		ResponseStructure<Student> res = new ResponseStructure<>();
+
+		res.setStatusCode(HttpStatus.OK.value());
+		res.setMessage("Student found");
+		res.setData(studentService.findStudentById(id));
+
+		return new ResponseEntity<>(res, HttpStatus.OK);
+	}
+
+	// update using put (full update)
+	@PutMapping("/{id}")
+	public ResponseEntity<ResponseStructure<Student>> updateStudent(@PathVariable Integer id,
+			@Valid @RequestBody Student student) {
+
+		ResponseStructure<Student> res = new ResponseStructure<>();
+
+		res.setStatusCode(HttpStatus.OK.value());
+		res.setMessage("Student updated");
+		res.setData(studentService.updateStudent(id, student));
+
+		return new ResponseEntity<>(res, HttpStatus.OK);
+	}
+
+	// Delete Single student by ID
+	@DeleteMapping("/{id}")
+	public ResponseEntity<ResponseStructure<String>> deleteStudent(@PathVariable int id) {
+		ResponseStructure<String> res = new ResponseStructure<>();
+
+		res.setStatusCode(HttpStatus.OK.value()); // 200
+		res.setMessage("Single deletion processing complete");
+		res.setData(studentService.deleteStudentById(id));
+
+		return new ResponseEntity<>(res, HttpStatus.OK);
 	}
 
 }
--- PageFlow/src/main/java/com/PageFlow/service/StudentService.java
+++ PageFlow/src/main/java/com/PageFlow/service/StudentService.java
@@ -1,9 +1,13 @@
 package com.PageFlow.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.PageFlow.entity.Student;
+import com.PageFlow.exception.IdNotFoundException;
+import com.PageFlow.exception.NoRecordAvailableException;
 import com.PageFlow.repository.StudentRepository;
 
 @Service
@@ -24,6 +28,48 @@ public class StudentService {
 
 		return studentRepository.save(student);
 
+	}
+
+	// fetch all student
+	public List<Student> getAllStudents() {
+		List<Student> students = studentRepository.findAll();
+
+		if (students.isEmpty()) {
+			throw new NoRecordAvailableException("No students available");
+		}
+
+		return students;
+	}
+
+	// Fetch by id
+	public Student findStudentById(int id) {
+		return studentRepository.findById(id).orElseThrow(
+				() -> new IdNotFoundException("Student with ID " + id + " does not exist in the library."));
+	}
+
+	// update
+	public Student updateStudent(Integer id, Student student) {
+
+		Student existingStudent = studentRepository.findById(id)
+				.orElseThrow(() -> new IdNotFoundException("Student with ID " + id + " does not exist"));
+
+		existingStudent.setName(student.getName());
+		existingStudent.setEmail(student.getEmail());
+		existingStudent.setPhone(student.getPhone());
+		existingStudent.setRollNumber(student.getRollNumber());
+		existingStudent.setDepartment(student.getDepartment());
+		existingStudent.setBatch(student.getBatch());
+
+		return studentRepository.save(existingStudent);
+	}
+
+	// Delete by ID
+	public String deleteStudentById(Integer id) {
+		Student student = studentRepository.findById(id).orElseThrow(
+				() -> new IdNotFoundException("Student with ID " + id + " does not exist in the library."));
+		studentRepository.delete(student);
+
+		return "Student with ID " + id + " deleted successfully.";
 	}
 
 }
